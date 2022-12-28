64e69935731bce36bc16cc765f19741fdea9865e
jfair1212 <83917954+jfair1212@users.noreply.github.com>
2022-12-28 12:46:39 -0500

    Initial Commit

---
+++ script.js
@@ -0,0 +1,17 @@
+var chart = document.getElementsByClassName("chart")
+var popup = document.getElementsByClassName("popup")
+
+for ( let i = 0; i < chart.length; i++ ) {
+    chart[i].addEventListener("mouseover", function() {
+        number = i;
+        popup[number].style.display = "inline-block"
+    })
+}
+
+for ( let i = 0; i < chart.length; i++ ) {
+    chart[i].addEventListener("mouseout", function() {
+        number = i;
+        popup[number].style.display = "none"
+    })
+}
+
