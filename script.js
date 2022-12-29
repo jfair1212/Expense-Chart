d65e46b84993673c0c31b2b97fbb7310d802e314
jfair1212 <83917954+jfair1212@users.noreply.github.com>
2022-12-29 08:44:45 -0500

    Viewport width adjustments
    In regards to body width, I switched from media queries to use the min() method in CSS

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
-var chart = document.getElementsByClassName("chart")
-var popup = document.getElementsByClassName("popup")
+var rankings = document.getElementsByName("ranking");
+var main = document.getElementById("main-div");
+var thankYou = document.getElementById("thank-you-state");
+var submit = document.getElementById("submit");
+var rateSelection = document.getElementById("rate-selection")
+var error = document.getElementById("error")
 
-for ( let i = 0; i < chart.length; i++ ) {
-    chart[i].addEventListener("mouseover", function() {
-        number = i;
-        popup[number].style.display = "inline-block"
-    })
+
+
+submit.onclick = function() {
+    for ( i = 0; i < rankings.length; i++ ) {
+        if ( rankings[i].checked ) {
+            selection = document.querySelector('input[name="ranking"]:checked').value;
+            error.style.display = "none";
+            main.style.display = "none";
+            rateSelection.innerHTML = `You chose ${selection} out of 5 stars`;
+            thankYou.style.display = "flex"
+            return true;
+        }
+    }
+    error.style.display = "flex";
+    return false;
 }
 
-for ( let i = 0; i < chart.length; i++ ) {
-    chart[i].addEventListener("mouseout", function() {
-        number = i;
-        popup[number].style.display = "none"
-    })
-}
+
 
